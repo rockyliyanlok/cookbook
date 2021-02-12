00f54384c8ac04e9f17ba016ddaaab37c1415cb4
Rocky Li <rocky.li@scmp.com>
2021-02-13 01:44:01 +0800

    i4-build-recipe-cards

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
 import _get from 'lodash/get'
 import axios from 'axios'
 import getConfig from 'next/config'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RecipeCards from '../components/RecipeCards'
 
 const { publicRuntimeConfig: { SPOONACULAR_APIKEY, CACHED_SPOONACULAR_RECIPE_SEARCH_URI } } = getConfig()
 
-const Index = ({
-  recipes
-}) => (
-  <RecipeCards recipes={recipes} />
-)
+const Index = () => {
+  const [recipes, setRecipes] = useState([])
 
-export async function getServerSideProps () {
-  const response = await axios(CACHED_SPOONACULAR_RECIPE_SEARCH_URI, {
-    params: {
-      apiKey: SPOONACULAR_APIKEY,
-      number: 24
-    }
-  })
-  const recipes = _get(response, 'data.results', [])
+  useEffect(() => {
+    (async () => { 
+      const response = await axios(CACHED_SPOONACULAR_RECIPE_SEARCH_URI, {
+        params: {
+          apiKey: SPOONACULAR_APIKEY,
+          number: 24
+        }
+      })
+      setRecipes(_get(response, 'data.results', []))
+    })()
 
-  return {
-    props: {
-      recipes
-    }
-  }
+    return () => {}
+  }, [])
+
+  return (
+    <RecipeCards recipes={recipes} />
+  )
 }
 
 export default Index
