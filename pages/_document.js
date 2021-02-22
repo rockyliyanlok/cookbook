dd99aaa42cce703de767387e7be60855d7383e41
Rocky Li <rocky.li@scmp.com>
2021-02-23 01:21:28 +0800

    i13-create-opensearch-xml

--- pages/_document.js
+++ pages/_document.js
@@ -45,6 +45,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,900;1,400&display=swap" rel="stylesheet" />
           <meta name="theme-color" content="#2D4558"/>
+          <link rel="search" href="/opensearch.xml" title="Search Cookbook" type="application/opensearchdescription+xml" />
         </Head>
         <body>
           <Main />
